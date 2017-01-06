5c03a12e0b456fd0ed2ef9732a85cca7fa3e3432
Dhananjayan Santhanakrishnan <sdjcse1@gmail.com>
2017-01-06 16:21:41 -0700

    changed logic to use double ended linked list

--- src/com/coursera/algorithms/week2/Deque.java
+++ src/com/coursera/algorithms/week2/Deque.java
@@ -1,5 +1,7 @@
 package com.coursera.algorithms.week2;
 
+import sun.awt.image.ImageWatched;
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
@@ -8,47 +10,53 @@ import java.util.NoSuchElementException;
  */
 public class Deque<Item> implements Iterable<Item>
 {
-    private int current,end,start;
-    Item[] holder = null;
+    private class LinkedList<Item>
+    {
+        Item data;
+        LinkedList<Item> next;
+        LinkedList<Item> prev;
+
+        LinkedList(Item data)
+        {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+        LinkedList(Item data,LinkedList next,LinkedList prev)
+        {
+            this.data = data;
+            this.next = next;
+            this.prev = prev;
+        }
+        public Item getData()
+        {
+            return data;
+        }
+        public LinkedList getPrev()
+        {
+            return this.prev;
+        }
+        public LinkedList getNext()
+        {
+            return this.next;
+        }
+    }
+    private LinkedList<Item> startPtr,endPtr;
+    private int size;
     public Deque()                           // construct an empty deque
     {
-        start = 0;
-        end = 0;
-        holder = (Item [])new Object[1];
+        this.startPtr = null;
+        this.endPtr = null;
+        this.size = 0;
     }
 
-    // Method to increase size
-    private void increaseSize()
-    {
-        int size = holder.length;
-        Item[] newHolder = (Item [])new Object[size*2];
-        for(int i=0;i<size;i++){
-            newHolder[i] = holder[i];
-        }
-        holder = newHolder;
-    }
-
-    // Method to reduce size
-    private void reduceSize()
-    {
-        if(end-start > (holder.length/4))
-        {
-            return;
-        }
-        Item[] newHolder = (Item [])new Object[holder.length/2];
-        for(int i=0;i<end-start;i++)
-        {
-            newHolder[i] = holder[i];
-        }
-        holder = newHolder;
-    }
     public boolean isEmpty()                 // is the deque empty?
     {
-        return end-start==0;
+        return startPtr==null;
     }
     public int size()                        // return the number of items on the deque
     {
-        return end-start;
+        return size;
     }
     public void addFirst(Item item)          // add the item to the front
     {
@@ -56,16 +64,15 @@ public class Deque<Item> implements Iterable<Item>
         {
             throw new NullPointerException();
         }
-        if(this.isEmpty())
+        LinkedList temp = this.startPtr;
+        this.startPtr = new LinkedList(item,startPtr,null);
+        if(this.endPtr==null)
         {
-            start = 0;
-            end = 1;
-            holder[start] = item;
+            this.endPtr = this.startPtr;
+        }else{
+            temp.prev = this.startPtr;
         }
-        else
-        {
-
-        }
+        this.size++;
     }
     public void addLast(Item item)           // add the item to the end
     {
@@ -73,39 +80,111 @@ public class Deque<Item> implements Iterable<Item>
         {
             throw new NullPointerException();
         }
+        LinkedList temp = endPtr;
+        endPtr = new LinkedList(item,null,endPtr);
 
+        if(startPtr==null)
+        {
+            startPtr = endPtr;
+        }else{
+            temp.next = endPtr;
+        }
+        size++;
     }
     public Item removeFirst()                // remove and return the item from the front
     {
-        return generalizedRemove(start,true);
+        return generalizedRemove(true);
     }
     public Item removeLast()                 // remove and return the item from the end
     {
-        return generalizedRemove(start,false);
+        return generalizedRemove(false);
     }
 
-    private Item generalizedRemove(int index,boolean isStart)
+    private Item generalizedRemove(boolean isStart)
     {
         if(this.isEmpty())
         {
             throw new NoSuchElementException();
         }
-        Item toBeReturned = holder[index];
-        holder[index] = null;
-        start = isStart ? ++start : start;
-        end = !(isStart) ? --end : end;
-        return  toBeReturned;
+        Item temp = (Item) (isStart ? startPtr.getData() : endPtr.getData());
+        if(isStart)
+        {
+            startPtr = startPtr.getNext();
+            startPtr.prev = null;
+        }
+        else
+        {
+            endPtr = endPtr.getPrev();
+            endPtr.next = null;
+        }
+        size--;
+        return temp;
+    }
+    private class DequeIterator<Item> implements Iterator<Item>
+    {
+        private final  Deque obj;
+        private LinkedList<Item> ptr;
+        DequeIterator(Deque obj)
+        {
+            this.obj = obj;
+            this.ptr = obj.startPtr;
+        }
+        @Override
+        public boolean hasNext()
+        {
+            return (ptr!=null);
+        }
+
+        @Override
+        public  void remove()
+        {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public Item next()
+        {
+            Item temp = null;
+            if(this.hasNext())
+            {
+                temp = ptr.data;
+                ptr = ptr.next;
+            }
+            return temp;
+        }
     }
     public Iterator<Item> iterator()         // return an iterator over items in order from front to end
     {
-        return (Iterator<Item>) new Object();
-    }
-    public void remove()
-    {
-        throw new UnsupportedOperationException();
+        return new DequeIterator<Item>(this);
     }
     public static void main(String[] args)   // unit testing
     {
-
+        Deque<Integer> testObj = new Deque<>();
+        testObj.addFirst(10);
+        testObj.addFirst(100);
+        testObj.addFirst(1048);
+        testObj.addFirst(489);
+        testObj.addLast(153);
+        testObj.addLast(15);
+        testObj.addLast(20);
+        Iterator iter = testObj.iterator();
+        while(iter.hasNext())
+        {
+            System.out.println(iter.next());
+        }
+        System.out.println(testObj.size());
+        System.out.println();
+        System.out.println(testObj.removeFirst());
+        System.out.println(testObj.removeFirst());
+        System.out.println(testObj.removeLast());
+        System.out.println(testObj.removeLast());
+        System.out.println(testObj.size());
+        System.out.println();
+        iter = testObj.iterator();
+        while(iter.hasNext())
+        {
+            System.out.println(iter.next());
+        }
+        System.out.println(testObj.size());
     }
 }
