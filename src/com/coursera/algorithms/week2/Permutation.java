a0602f3458470500ef000fd94fbe9a240694554b
Dhananjayan Santhanakrishnan <sdjcse1@gmail.com>
2017-01-09 12:36:23 -0700

    completed all cases 93% - third quest

--- src/com/coursera/algorithms/week2/Permutation.java
+++ src/com/coursera/algorithms/week2/Permutation.java
@@ -1,8 +1,24 @@
 package com.coursera.algorithms.week2;
 
+import edu.princeton.cs.algs4.StdIn;
+
 /**
- * Created by sdj on 1/4/17.
+ * Created by sdj on 1/6/17.
  */
 public class Permutation
 {
+    public static void main(String[] args)
+    {
+        RandomizedQueue<String> rq = new RandomizedQueue();
+        int k = Integer.parseInt(args[0]);
+        while(!StdIn.isEmpty())
+        {
+            rq.enqueue(StdIn.readString());
+        }
+        while(k>0)
+        {
+            System.out.println(rq.dequeue());
+            k--;
+        }
+    }
 }
