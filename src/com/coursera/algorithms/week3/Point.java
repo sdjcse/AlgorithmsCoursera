1f9206fe70806c0947c432881c64f643d7ca1b21
Dhananjayan Santhanakrishnan <sdjcse1@gmail.com>
2017-01-15 00:05:06 -0700

    partial complete

--- src/com/coursera/algorithms/week3/Point.java
+++ src/com/coursera/algorithms/week3/Point.java
@@ -8,9 +8,18 @@ import java.util.Comparator;
 public class Point
 {
     int x,y ;
+    public int getX()
+    {
+        return this.x;
+    }
+    public int getY()
+    {
+        return this.y;
+    }
     public Point(int x, int y)                         // constructs the point (x, y)
     {
-
+        this.x = x;
+        this.y = y;
     }
 
     public   void draw()                               // draws this point
@@ -26,16 +35,38 @@ public class Point
         return "x:" + Integer.toString(x) + ", y: " + Integer.toString(y);
     }
 
-    public               int compareTo(Point that)     // compare two points by y-coordinates, breaking ties by x-coordinates
+    public int compareTo(Point that)     // compare two points by y-coordinates, breaking ties by x-coordinates
     {
-        return 0;
+        return (this.getY() < that.getY() || (this.getY() == that.getY() && this.getX() < that.getX())) ? -1 : (this.getX() == that.getX() && this.getY() == that.getY()) ? 0 : +1;
     }
     public            double slopeTo(Point that)       // the slope between this point and that point
     {
-        return 0;
+        double nr = (double) (that.getY()-this.getY());
+        double dr = (double) (that.getX()-this.getX());
+
+        if(nr==0 && dr == 0)
+        {
+            return -Double.MAX_VALUE;
+        }
+        else if(nr==0)
+        {
+            return +0;
+        }
+        else if(dr==0)
+        {
+            return Double.MAX_VALUE;
+        }
+        return nr/dr;
     }
     public Comparator<Point> slopeOrder()              // compare two points by slopes they make with this point
     {
-        return null;
+        return new Comparator<Point>()
+        {
+            @Override
+            public int compare(Point point, Point t1)
+            {
+                return Double.compare(slopeTo(point),slopeTo(t1));
+            }
+        };
     }
 }
